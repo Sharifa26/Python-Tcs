7d118512ff5bff589b6ccf2a085ca2fdae067ac6
Sharifa26 <ismailsheriff2001@gmail.com>
2023-11-02 14:44:37 +0530

    second

--- ARRAYS/8-containsDuplicate.py
+++ ARRAYS/8-containsDuplicate.py
@@ -1,11 +1,12 @@
+# https://leetcode.com/problems/contains-duplicate/submissions/
 nums = [1,2,3,1]
 
 def contains(nums):
-    n=len(nums)
-    for i in range(n-1):
-        for j in range(i+1,n):
-            if(nums[i] == nums[j]):
-             return True
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
     return False
         
 print(contains(nums))
